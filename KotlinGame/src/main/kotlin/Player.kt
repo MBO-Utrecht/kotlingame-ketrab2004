763d85fa23795d9e87465af59a67a1e2c38f8619
ketrab2004 <bartek@fos.nl>
2021-03-18 11:52:06 +0100

    Changed weapon to primary

--- KotlinGame/src/main/kotlin/Player.kt
+++ KotlinGame/src/main/kotlin/Player.kt
@@ -1,7 +1,7 @@
 class Player(val name: String, val maxLife: Int = 3, var level: Int = 1, var score: Int = 0) {
     var life: Int = maxLife;
 
-    var weapon: Weapon = Weapon("Stick","a Small stick", 1);
+    var primary: Weapon = Weapon("Stick","a Small stick", 1);
 
     var coins: Int = 0;
 
@@ -14,9 +14,9 @@ class Player(val name: String, val maxLife: Int = 3, var level: Int = 1, var sco
             coins: $coins
             
             Weapon:
-                name: ${weapon.name}
-                description: ${weapon.description}
-                damage: ${weapon.damage}
+                name: ${primary.name}
+                description: ${primary.description}
+                damage: ${primary.damage}
             """.trimIndent())
     }
 }
