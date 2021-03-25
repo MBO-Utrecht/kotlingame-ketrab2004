6ea8b58c6a13cad8bbf3fc4dfc52af3d121c8bdd
ketrab2004 <bartek@fos.nl>
2021-03-25 12:56:30 +0100

    Added more lootTypes and toString override

--- KotlinGame/src/main/kotlin/Loot.kt
+++ KotlinGame/src/main/kotlin/Loot.kt
@@ -1,8 +1,17 @@
 enum class LootType {
-    KEY, JEWELRY
+    KEY, JEWELRY, ARMOR, RING, POTION
 }
 
-class Loot (val name:String, val description:String, val type: LootType ) {
+class Loot (val name:String, val type: LootType, val worth: Float ) {
+    val description:String = "cool item";
 
 
+    override fun toString(): String {
+        return """
+            name:  $name
+            description:  $description
+            type: $type
+            worth: $worth
+            """.trimIndent()
+    }
 }
