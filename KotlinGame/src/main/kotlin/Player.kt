08320ee21a8e7c7e7b3eaf6c694638e1d99ee7bc
ketrab2004 <bartek@fos.nl>
2021-03-25 12:57:13 +0100

    Replaced .show with toString override

--- KotlinGame/src/main/kotlin/Player.kt
+++ KotlinGame/src/main/kotlin/Player.kt
@@ -1,22 +1,30 @@
 class Player(val name: String, val maxLife: Int = 3, var level: Int = 1, var score: Int = 0) {
     var life: Int = maxLife;
 
-    var primary: Weapon = Weapon("Stick","a Small stick", 1);
+    var primary: Weapon = Weapon("Stick", 1);
+
+    var inventory: ArrayList<Loot> = ArrayList();
 
     var coins: Int = 0;
 
-    fun show() {
-        println("""
-            name:  $name
-            life:  $life
-            level: $level
-            score: $score
-            coins: $coins
-            
-            Weapon:
-                name: ${primary.name}
-                description: ${primary.description}
-                damage: ${primary.damage}
-            """.trimIndent())
+    override fun toString(): String {
+        return """
+name:  $name
+life:  $life
+level: $level
+score: $score
+coins: $coins
+
+* Weapon:
+$primary """
+    }
+
+    fun showInventory() {
+        println("* Inventory:")
+        for ((i, item) in inventory.withIndex()) {
+            println(
+"""=== $i:
+$item""")
+        }
     }
 }
