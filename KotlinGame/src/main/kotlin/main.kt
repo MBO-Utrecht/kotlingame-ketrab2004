b615c258eb74adfc84fbaa3cb713ec0256981d3d
ketrab2004 <bartek@fos.nl>
2021-03-25 12:56:55 +0100

    Added 2 items and changed .show to .toString

--- KotlinGame/src/main/kotlin/main.kt
+++ KotlinGame/src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
     val player = Player("Plyr", 3, 1, 0)
 
-    player.show()
+    player.inventory.add(Loot("red potion", LootType.POTION, 2.5f))
+    player.inventory.add(Loot("helmet", LootType.ARMOR, 50.5f))
+
+    println(player);
+    println("")
+    player.showInventory();
 }
