87ab231373499f2c2573101395866f7b8e42cfca
ketrab2004 <bartek@fos.nl>
2021-03-25 12:57:37 +0100

    Added toString override

--- KotlinGame/src/main/kotlin/Weapon.kt
+++ KotlinGame/src/main/kotlin/Weapon.kt
@@ -1,4 +1,11 @@
-class Weapon (val name: String, val description: String, val damage: Int) {
+class Weapon (val name: String, val damage: Int) {
+    val description: String = "owww"
 
-
+    override fun toString(): String {
+        return """
+            name:  $name
+            description:  $description
+            damage: $damage
+            """.trimIndent()
+    }
 }
