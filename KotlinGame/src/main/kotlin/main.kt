69d8bc61784eae6aac2ad07447e6746d79284679
ketrab2004 <bartek@fos.nl>
2021-04-01 12:59:34 +0200

    Created player 2 with inventory Also for loop with dividable by 3/5 check

--- KotlinGame/src/main/kotlin/main.kt
+++ KotlinGame/src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val player = Player("Plyr", 3, 1, 0)
+    /*val player = Player("Plyr", 3, 1, 0)
 
     player.inventory.add(Loot("red potion", LootType.POTION, 2.5f))
     player.inventory.add(Loot("helmet", LootType.ARMOR, 50.5f))
@@ -7,4 +7,30 @@ fun main(args: Array<String>) {
     println(player);
     println("")
     player.showInventory();
+    println(""); println("");
+    */
+
+
+    val player2 = Player("Player 2", 3, 1, 0)
+    player2.inventory.add(  Loot("helmet", LootType.ARMOR, 45f) )
+    player2.inventory.add(  Loot("blue potion", LootType.POTION, 25f)   )
+
+    player2.inventory.add(  Loot("ring 1", LootType.RING, 15f)  )
+    player2.inventory.add(  Loot("ring 2", LootType.RING, 14.99f)   )
+
+    player2.showInventory()
+
+    println(""); println("");
+
+    for (i in 100 downTo 0)
+    {
+        if (i % 3 == 0 && i % 5 == 0){
+            println("$i is deelbaar door 3 en 5")
+        }else if (i % 3 == 0){
+            println("$i is deelbaar door 3")
+        }else if (i % 5 == 0){
+            println("$i is deelbaar door 5")
+        }
+    }
+
 }
